3360bec5cea4cefacc360779f190f1d947cbd2a1
Killian Provin <killian.provin@telecom-paris.fr>
2024-12-17 23:11:41 +0100

    test partie 6

--- main.cpp
+++ main.cpp
@@ -15,17 +15,17 @@ int main(int argc, const char* argv[])
 {
     std::cout << "Hello brave new world" << std::endl;
 
-    Multimedia* photo1 = new Photo("photo1", "./media/photo1.png");
-    Multimedia* photo2 = new Photo("photo2", "./media/photo2.png");
-    Multimedia* photo3 = new Photo("photo3", "./media/photo3.png");
-    Multimedia* video1 = new Video("video1", "./media/video1.mov");
-    Multimedia* video2 = new Video("video2", "./media/video2.mov");
+    Film* film = new Film("Film", "./media/video1.mov", 120, new int[3]{10, 20, 30}, 3);
 
-    Multimedia* media[5] = {photo1, video1, photo2, photo3, video2};
+    film->display(std::cout);
+    film->setChapterDurations(new int[2]{15, 25}, 2);
+    film->setDuration(150);
+    film->display(std::cout);
+    film->setChapterDurations({}, 0);
+    film->setDuration(150);
+    film->display(std::cout);
 
-    for (int i = 0; i < 5; i++) {
-        media[i]->display(cout);
-    }
+    delete film;
 
     return 0;
 }  
